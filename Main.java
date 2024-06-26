910df3fbcc52f3d3756717914a820793df17abed
Amin <mohammad1383620@gmail.com>
2024-06-26 23:28:14 +0330

    In the name of god  nbhjh

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("In the name     of gad");
+        System.out.println("In the     name     of gad");
     }
 }
